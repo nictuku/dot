0185c2849afd8feac83cfdb846868687c82ce4a1
Yves Junqueira <yves.junqueira@gmail.com>
2016-01-25 01:20:00 -0800

    Update git.sh

--- git.sh
+++ git.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 git config --global user.name "Yves Junqueira"
-git config --global user.name "yves.junqueira@gmail.com"
+git config --global user.email "yves.junqueira@gmail.com"
 
 ssh-keygen -t dsa -f ~/.ssh/id_dsa -N ""
