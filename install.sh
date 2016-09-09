b122a1d4ce76476028b7966ff2a6787b3e70dc4f
Yves Junqueira <yves.junqueira@gmail.com>
2016-09-09 04:01:12 +0000

    install vim-go

--- install.sh
+++ install.sh
@@ -23,3 +23,5 @@ done
 # Pathogen vim plugin manager
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+# vim-go
+git clone https://github.com/fatih/vim-go.git ~/.vim/pack/plugins/start/vim-go
