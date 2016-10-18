23a77fdabaada2e77120b33521ac0422863192e6
Yves Junqueira <yves.junqueira@gmail.com>
2016-10-18 12:50:09 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -25,3 +25,4 @@ mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 # vim-go
 git clone https://github.com/fatih/vim-go.git ~/.vim/pack/plugins/start/vim-go
+git clone git://github.com/ntpeters/vim-better-whitespace.git ~/.vim/bundle/vim-better-whitespace
