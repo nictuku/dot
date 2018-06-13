c97d9f798ec565f54bd2cb84078002480f76a648
Yves Junqueira <yves.junqueira@gmail.com>
2018-06-13 10:35:59 -0700

    Create deps.sh

---
+++ deps.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -eux
+GO_VERSION=1.10.1
+sudo apt update
+sudo apt install realpath git screen vim curl sudo mosh mercurial -y
+cd /tmp
+wget https://storage.googleapis.com/golang/go${GO_VERSION}.linux-amd64.tar.gz
+if [[ -d /usr/local/go]] ; then sudo mv /usr/local/go{,.before-${GO_VERSION}}; fi
+sudo tar zxvf go${GO_VERSION}.linux-amd64.tar.gz  -C /usr/local/
