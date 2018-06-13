bbdba25309e66de2417d7c2b9b360be5fd267dd0
Yves Junqueira <yves.junqueira@gmail.com>
2018-06-13 10:41:00 -0700

    Update deps.sh

--- deps.sh
+++ deps.sh
@@ -6,5 +6,5 @@ sudo apt update
 sudo apt install realpath git screen vim curl sudo mosh mercurial -y
 cd /tmp
 wget https://storage.googleapis.com/golang/go${GO_VERSION}.linux-amd64.tar.gz
-if [[ -d /usr/local/go]] ; then sudo mv /usr/local/go{,.before-${GO_VERSION}}; fi
+if [[ -d /usr/local/go ]] ; then sudo mv /usr/local/go{,.before-${GO_VERSION}}; fi
 sudo tar zxvf go${GO_VERSION}.linux-amd64.tar.gz  -C /usr/local/
