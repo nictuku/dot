743d636a6204130e7c4f5f47918a27ed4d6bf008
Yves Junqueira <yves.junqueira@gmail.com>
2018-12-17 12:08:52 -0800

    Update deps.sh

--- deps.sh
+++ deps.sh
@@ -3,7 +3,7 @@
 set -eux
 GO_VERSION=1.11.4
 sudo apt update
-sudo apt install git screen vim curl sudo mosh mercurial -y
+sudo apt install git screen vim curl sudo mosh mercurial build-essential -y
 cd /tmp
 wget https://storage.googleapis.com/golang/go${GO_VERSION}.linux-amd64.tar.gz
 if [[ -d /usr/local/go ]] ; then sudo mv /usr/local/go{,.before-${GO_VERSION}}; fi
