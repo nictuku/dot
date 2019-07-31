48e53ae48a2890112d31b0198dd0b7b3ef7a2318
Yves Junqueira <yves.junqueira@gmail.com>
2019-07-31 16:04:33 -0700

    Update deps.sh

--- deps.sh
+++ deps.sh
@@ -2,9 +2,11 @@
 
 set -eux
 GO_VERSION=1.12.6
-sudo apt update
-sudo apt install git screen vim curl sudo mosh mercurial build-essential -y
-cd /tmp
-wget https://storage.googleapis.com/golang/go${GO_VERSION}.linux-amd64.tar.gz
-if [[ -d /usr/local/go ]] ; then sudo mv /usr/local/go{,.before-${GO_VERSION}}; fi
-sudo tar zxvf go${GO_VERSION}.linux-amd64.tar.gz  -C /usr/local/
+if [ -f "/etc/debian_version" ];
+  sudo apt update
+  sudo apt install git screen vim curl sudo mosh mercurial build-essential -y
+  cd /tmp
+  wget https://storage.googleapis.com/golang/go${GO_VERSION}.linux-amd64.tar.gz
+  if [[ -d /usr/local/go ]] ; then sudo mv /usr/local/go{,.before-${GO_VERSION}}; fi
+  sudo tar zxvf go${GO_VERSION}.linux-amd64.tar.gz  -C /usr/local/
+fi
