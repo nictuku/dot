e531518d3ac29fa5ff167316910821d135bae565
nictuku <yves.junqueira@gmail.com>
2022-12-25 17:51:34 +0000

    Update deps.sh

--- deps.sh
+++ deps.sh
@@ -8,5 +8,5 @@ if [ -f "/etc/debian_version" ]; then
   cd /tmp
   wget https://storage.googleapis.com/golang/go${GO_VERSION}.linux-amd64.tar.gz
   if [[ -d /usr/local/go ]] ; then sudo mv /usr/local/go{,.before-${GO_VERSION}}; fi
-  sudo tar zxvf go${GO_VERSION}.linux-amd64.tar.gz  -C /usr/local/
+  sudo tar zxf go${GO_VERSION}.linux-amd64.tar.gz  -C /usr/local/
 fi
