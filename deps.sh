56fb363ffbad97ddce488ca7a0c28369c15a4d0f
nictuku <yves.junqueira@gmail.com>
2023-04-22 18:58:47 +0100

    Update deps.sh

--- deps.sh
+++ deps.sh
@@ -4,7 +4,8 @@ set -eux
 GO_VERSION=1.19.1
 if [ -f "/etc/debian_version" ]; then
   sudo apt update
-  sudo apt install git screen wget vim curl sudo mosh mercurial build-essential -y
+  # nodejs is needed by vim-copilot
+  sudo apt install git screen wget vim curl sudo mosh mercurial build-essential nodejs -y
   cd /tmp
   wget https://storage.googleapis.com/golang/go${GO_VERSION}.linux-amd64.tar.gz
   if [[ -d /usr/local/go ]] ; then sudo mv /usr/local/go{,.before-${GO_VERSION}}; fi
