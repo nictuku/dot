b6f0debeef17da1d1abc5d72622729aadc8f0fab
nictuku <yves.junqueira@gmail.com>
2023-09-02 15:09:54 +0100

    Update deps.sh

--- deps.sh
+++ deps.sh
@@ -8,7 +8,7 @@ if [ -f "/etc/debian_version" ]; then
   sudo -E apt update
   sudo -E apt install -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" git screen wget vim curl sudo mosh mercurial build-essential nodejs jq
   cd /tmp
-  GO_VERSION="$(curl https://golang.org/dl/?mode=json | jq -r '.[0].version')"
+  GO_VERSION="$(curl https://go.dev/dl/?mode=json | jq -r '.[0].version')"
   wget https://storage.googleapis.com/golang/go${GO_VERSION}.linux-amd64.tar.gz
   if [[ -d /usr/local/go ]] ; then sudo mv /usr/local/go{,.before-${GO_VERSION}}; fi
   sudo tar zxf go${GO_VERSION}.linux-amd64.tar.gz  -C /usr/local/
